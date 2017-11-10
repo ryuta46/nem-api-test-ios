731a7b57d727e264798670aad689988e10dbb901
Taizo Kusuda <taizok2@gmail.com>
2017-11-11 06:08:44 +0900

    initial import

---
+++ NemApiTestiOS/NemApiTestiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NemApiTestiOS
+//
+//  Created by Taizo Kusuda on 2017/11/11.
+//  Copyright © 2017年 ryuta46. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
