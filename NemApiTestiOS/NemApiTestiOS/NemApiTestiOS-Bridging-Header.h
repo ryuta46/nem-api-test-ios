8bf24a834fd17947a5e0a1684453006931c110c3
Taizo Kusuda <taizok2@gmail.com>
2017-11-11 07:42:45 +0900

    add libraies as submodule

---
+++ NemApiTestiOS/NemApiTestiOS/NemApiTestiOS-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  NemApiTestiOS-Bridging-Header.h
+//  NemApiTestiOS
+//
+//  Created by Taizo Kusuda on 2017/11/11.
+//  Copyright © 2017年 ryuta46. All rights reserved.
+//
+
+#ifndef NemApiTestiOS_Bridging_Header_h
+#define NemApiTestiOS_Bridging_Header_h
+
+#import "RHash/librhash/sha3.h"
+#import "ed25519/src/ed25519.h"
+
+
+#endif /* NemApiTestiOS_Bridging_Header_h */
